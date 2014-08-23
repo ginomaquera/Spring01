4e82afd80257e3055e7fb7031808d85fec3cb975
SABADO-MANHANA <SABADO-MANHANA@LAB707-06>
2014-08-23 13:56:48 -0500

    6

--- clase/src/main/java/com/spring/AppSpringHibernate.java
+++ clase/src/main/java/com/spring/AppSpringHibernate.java
@@ -18,10 +18,38 @@ public class AppSpringHibernate {
 
     public static void main(String[] args) {
         ApplicationContext cntx = new ClassPathXmlApplicationContext("h_db.xml");
-        ProgramaDAO localDAO = (ProgramaDAO) cntx.getBean("programaDAO");        
+        
+        ProgramaDAO localDAO = (ProgramaDAO) cntx.getBean("programaDAO");
+        Programa programa = new Programa(9L, "curso 26", "curso 26", "26");
+        //AppSpringHibernate.doInsert(cntx, programa); 
+        //Programa programa1 = localDAO.get(9L);
+        //programa1.setDescripcion("33");
+        localDAO.update(programa);
+        //AppSpringHibernate.doUpdate(cntx, programa1);
+        //AppSpringHibernate.doList(cntx);
         List<Programa> lista = localDAO.list();
         for (Programa p : lista) {
-            System.out.println("Nombre: " + p.getNombre() + " id: " + p.getCodigo());
+            System.out.println("Nombre: " + p.getNombre() + " id: "
+                    + p.getId());
         }
+    }
+
+    public static void doList(ApplicationContext context) {
+        ProgramaDAO localDAO = (ProgramaDAO) context.getBean("programaDAO");
+        List<Programa> lista = localDAO.list();
+        for (Programa p : lista) {
+            System.out.println("Nombre: " + p.getNombre() + " id: "
+                    + p.getId());
+        }
+    }
+    
+    public static void doInsert(ApplicationContext context, Programa programa) {
+        ProgramaDAO localDAO = (ProgramaDAO) context.getBean("programaDAO");
+        localDAO.save(programa);
+    }
+    
+    public static void doUpdate(ApplicationContext context, Programa programa) {
+        ProgramaDAO localDAO = (ProgramaDAO) context.getBean("programaDAO");
+        localDAO.update(programa); 
     }
 }
